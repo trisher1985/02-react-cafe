9faf4bed1e18c93a0e070e096c2aa3a8182d5a21
YuraHez <trisher1985@gmail.com>
2025-05-19 17:58:44 +0300

    added all components

---
+++ src/components/CafeInfo/CafeInfo.tsx
@@ -0,0 +1,12 @@
+import css from './CafeInfo.module.css';
+
+const CafeInfo = () => (
+  <div className={css.container}>
+    <h1 className={css.title}>Sip Happens Café</h1>
+    <p className={css.description}>
+      Please rate our service by selecting one of the options below.
+    </p>
+  </div>
+);
+
+export default CafeInfo;
